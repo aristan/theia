56a33bdd45342212639175f2e9b0b957b75c9fad
vince-fugnitto <vincent.fugnitto@ericsson.com>
2020-07-24 11:42:00 -0400

    scm: add api integration tests
    the following commit includes api integration tests for `scm`.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- packages/scm/src/browser/scm-widget.tsx
+++ packages/scm/src/browser/scm-widget.tsx
@@ -36,11 +36,11 @@ export class ScmWidget extends BaseWidget implements StatefulWidget {
 
     static ID = 'scm-view';
 
-    @inject(ScmService) protected readonly scmService: ScmService;
+    @inject(ScmService) readonly scmService: ScmService;
     @inject(ScmCommitWidget) protected readonly commitWidget: ScmCommitWidget;
-    @inject(ScmTreeWidget) protected readonly resourceWidget: ScmTreeWidget;
+    @inject(ScmTreeWidget) readonly resourceWidget: ScmTreeWidget;
     @inject(ScmAmendWidget) protected readonly amendWidget: ScmAmendWidget;
-    @inject(ScmNoRepositoryWidget) protected readonly noRepositoryWidget: ScmNoRepositoryWidget;
+    @inject(ScmNoRepositoryWidget) readonly noRepositoryWidget: ScmNoRepositoryWidget;
     @inject(ScmPreferences) protected readonly scmPreferences: ScmPreferences;
 
     set viewMode(mode: 'tree' | 'list') {
